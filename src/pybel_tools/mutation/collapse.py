b57e2085101ee1415512bcea65e7702e7dc891c2
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-09 11:08:28 -0400

    Fix edge dictionary size change during iteration

--- src/pybel_tools/mutation/collapse.py
+++ src/pybel_tools/mutation/collapse.py
@@ -354,7 +354,7 @@ def collapse_consistent_edges(graph):
 
     :param pybel.BELGraph graph: A BEL Graph
     """
-    for u, v in graph.edges_iter():
+    for u, v in graph.edges():
         relation = pair_is_consistent(graph, u, v)
 
         if not relation:
