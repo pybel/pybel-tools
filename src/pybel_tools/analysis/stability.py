008b600f6693f024e6eae7b4d2ae2bebead27c86
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-11 11:05:14 +0200

    Add summarize stability function

--- src/pybel_tools/analysis/stability.py
+++ src/pybel_tools/analysis/stability.py
@@ -25,7 +25,8 @@ __all__ = [
     'get_increase_mismatch_triplets',
     'get_decrease_mismatch_triplets',
     'get_chaotic_triplets',
-    'get_dampened_triplets'
+    'get_dampened_triplets',
+    'summarize_stability',
 ]
 
 log = logging.getLogger(__name__)
@@ -366,3 +367,40 @@ def get_dampened_triplets(graph):
     :rtype: iter[tuple]
     """
     return _get_disregulated_triplets_helper(graph, CAUSAL_DECREASE_RELATIONS)
+
+
+def summarize_stability(graph):
+    """Summarize the stability of the graph
+
+    :param pybel.BELGraph graph: A BEL graph
+    :rtype: dict
+    """
+
+    regulatory_pairs = get_regulatory_pairs(graph)
+    chaotic_pairs = get_chaotic_pairs(graph)
+    dampened_pairs = get_dampened_pairs(graph)
+    contraditory_pairs = get_contradiction_summary(graph)
+    separately_unstable_triples = get_separate_unstable_correlation_triples(graph)
+    mutually_unstable_triples = get_mutually_unstable_correlation_triples(graph)
+    jens_unstable_triples = get_jens_unstable(graph)
+    increase_mismatch_triples = get_increase_mismatch_triplets(graph)
+    decrease_mismatch_triples = get_decrease_mismatch_triplets(graph)
+    chaotic_triples = get_chaotic_triplets(graph)
+    dampened_triples = get_dampened_triplets(graph)
+
+    def count_or_len(it):
+        return sum(1 for _ in it)
+
+    return {
+        'Regulatory Pairs': count_or_len(regulatory_pairs),
+        'Chaotic Pairs': count_or_len(chaotic_pairs),
+        'Dampened Pairs': count_or_len(dampened_pairs),
+        'Contradictory Pairs': count_or_len(contraditory_pairs),
+        'Separately Unstable Triples': count_or_len(separately_unstable_triples),
+        'Mutually Unstable Triples': count_or_len(mutually_unstable_triples),
+        'Jens Unstable Triples': count_or_len(jens_unstable_triples),
+        'Increase Mismatch Triples': count_or_len(increase_mismatch_triples),
+        'Decrease Mismatch Triples': count_or_len(decrease_mismatch_triples),
+        'Chaotic Triples': count_or_len(chaotic_triples),
+        'Dampened Triples': count_or_len(dampened_triples)
+    }
