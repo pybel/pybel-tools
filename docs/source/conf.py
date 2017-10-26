8f6ae1f305f22718aa2eb33e98b2f6d866b92a22
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-10-26 22:57:18 +0200

    Bump version: 0.4.4 → 0.4.5-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ author = 'Charles Tapley Hoyt'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '0.4.4'
+release = '0.4.5-dev'
 
 parsed_version = re.match(
     '(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?:-(?P<release>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+(?P<build>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?',
