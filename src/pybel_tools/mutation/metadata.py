e6aa3aee5dcc1ecd163d61e11f2955004df055bb
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-07 21:10:48 +0100

    Revert "update logging"
    This reverts commit 1ce9d9ccfe355a0d1be6923dfa16673e8c8c0676.

--- src/pybel_tools/mutation/metadata.py
+++ src/pybel_tools/mutation/metadata.py
@@ -124,7 +124,7 @@ def enrich_pubmed_citations(graph, stringify_authors=False, manager=None):
         pmid = d[CITATION][CITATION_REFERENCE].strip()
 
         if pmid not in pmid_data:
-            log.debug('Missing data for PubMed identifier: %s', pmid)
+            log.warning('Missing data for PubMed identifier: %s', pmid)
             errors.add(pmid)
             continue
 
