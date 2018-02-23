65b19ba051bd56b30902dd8f6f6fe1984633cbe8
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-23 15:23:53 +0100

    Automatically make pickles

--- src/pybel_tools/io.py
+++ src/pybel_tools/io.py
@@ -4,14 +4,41 @@
 import logging
 import os
 
-from pybel import Manager, from_path, from_pickle, union
+from pybel import Manager, from_path, from_pickle, to_pickle, union
 
 __all__ = [
+    'from_path_ensure_pickle',
     'from_directory',
     'from_directory_pickles',
 ]
 
 log = logging.getLogger(__name__)
+
+_bel_extension = '.bel'
+_gpickle_extension = '.gpickle'
+
+
+def from_path_ensure_pickle(path, manager=None, **kwargs):
+    """Parses a path exactly like :func:`pybel.from_path` unless a corresponding .gpickle file is available
+
+    :param str path: A file path
+    :param manager: database connection string to cache, pre-built :class:`Manager`, or None to use default cache
+    :type manager: Optional[str or pybel.manager.Manager]
+    :param kwargs:
+    :rtype: pybel.BELGraph
+    """
+    if not path.endswith(_bel_extension):
+        raise ValueError
+
+    gpickle_path = path[:-len(_bel_extension)] + _gpickle_extension
+
+    if os.path.exists(gpickle_path):
+        return from_pickle(path)
+
+    graph = from_path(path, manager=manager ** kwargs)
+    to_pickle(graph, gpickle_path)
+
+    return graph
 
 
 def iter_pickle_paths_from_directory(directory):
@@ -99,6 +126,6 @@ def load_paths(paths, connection=None):
     manager = Manager.ensure(connection)
 
     return union(
-        from_path(path, manager=manager)
+        from_path_ensure_pickle(path, manager=manager)
         for path in paths
     )
