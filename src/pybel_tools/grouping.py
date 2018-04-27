5f0d7d1525ac055b1f50e6e7876940ebf4447894
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-27 11:28:51 +0200

    Add parameter for controlling if undefined edges are kepr

--- src/pybel_tools/grouping.py
+++ src/pybel_tools/grouping.py
@@ -18,11 +18,13 @@ __all__ = [
 
 
 @pipeline.splitter
-def get_subgraphs_by_annotation(graph, annotation, sentinel='Undefined'):
+def get_subgraphs_by_annotation(graph, annotation, keep_undefined=True, sentinel='Undefined'):
     """Stratifies the given graph into subgraphs based on the values for edges' annotations
 
     :param pybel.BELGraph graph: A BEL graph
     :param str annotation: The annotation to group by
+    :param bool keep_undefined: If true, uses the sentinel value to store a subgraph of edges not matching the given
+     annotation.
     :param str sentinel: The value to stick unannotated edges into
     :rtype: dict[str,pybel.BELGraph]
     """
@@ -31,7 +33,7 @@ def get_subgraphs_by_annotation(graph, annotation, sentinel='Undefined'):
     for source, target, key, data in graph.edges_iter(keys=True, data=True):
         annotation_dict = data.get(ANNOTATIONS)
 
-        if annotation_dict is None or annotation not in annotation_dict:
+        if (annotation_dict is None or annotation not in annotation_dict) and keep_undefined:
             safe_add_edge(result[sentinel], source, target, key, data)
         else:
             for value in annotation_dict[annotation]:
