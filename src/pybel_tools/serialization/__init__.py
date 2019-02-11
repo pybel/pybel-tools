47a69b608dfdd835a08e56dacd194f6bd55b954a
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-02-11 22:06:27 +0100

    Remove dead code

--- src/pybel_tools/serialization/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""This module contains alternative ways to functionalize BEL statements"""
-
-from . import functional
-from .functional import *
-
-__all__ = functional.__all__
