0d8fb8f13bf44d257a856fa5dacf35df66c5c6d7
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-03-15 09:30:04 +0100

    Update metadata.py

--- src/pybel_tools/mutation/metadata.py
+++ src/pybel_tools/mutation/metadata.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import logging
+"""Metadata mutation utilities."""
 
+import logging
 from typing import Set
 
+from pybel import BELGraph, Manager
 from pybel.constants import CITATION, CITATION_REFERENCE
 from pybel.manager.citation_utils import get_citations_by_pmids
-from pybel.struct.filters import filter_edges
-from pybel.struct.filters.edge_predicates import has_pubmed
+from pybel.struct.filters import filter_edges, has_pubmed
 from pybel.struct.pipeline import in_place_transformation, uni_in_place_transformation
-from pybel.struct.summary import get_pubmed_identifiers
-from pybel.struct.summary.node_summary import get_namespaces
-from ..summary.edge_summary import get_annotations
+from pybel.struct.summary import get_annotations, get_namespaces, get_pubmed_identifiers
 
 __all__ = [
     'enrich_pubmed_citations',
@@ -21,24 +20,11 @@ log = logging.getLogger(__name__)
 
 
 @in_place_transformation
-def enrich_pubmed_citations(graph, stringify_authors:bool=False, manager=None) -> Set[str]:
-    """Overwrites all PubMed citations with values from NCBI's eUtils lookup service.
+def enrich_pubmed_citations(graph: BELGraph, manager: Manager) -> Set[str]:
+    """Overwrite all PubMed citations with values from NCBI's eUtils lookup service.
 
-    Sets authors as list, so probably a good idea to run :func:`pybel_tools.mutation.serialize_authors` before
-    exporting.
-
-    :param pybel.BELGraph graph: A BEL graph
-    :param bool stringify_authors: Converts all author lists to author strings using
-                                  :func:`pybel_tools.mutation.serialize_authors`. Defaults to ``False``.
-    :param manager: An RFC-1738 database connection string, a pre-built :class:`pybel.manager.Manager`,
-                    or ``None`` for default connection
-    :type manager: None or str or Manager
     :return: A set of PMIDs for which the eUtils service crashed
     """
-    if 'PYBEL_ENRICHED_CITATIONS' in graph.graph:
-        log.warning('citations have already been enriched in %s', graph)
-        return set()
-
     pmids = get_pubmed_identifiers(graph)
     pmid_data, errors = get_citations_by_pmids(manager=manager, pmids=pmids)
 
@@ -52,18 +38,12 @@ def enrich_pubmed_citations(graph, stringify_authors:bool=False, manager=None) -
 
         graph[u][v][k][CITATION].update(pmid_data[pmid])
 
-    graph.graph['PYBEL_ENRICHED_CITATIONS'] = True
-
     return errors
 
 
 @uni_in_place_transformation
-def update_context(universe, graph):
-    """Updates the context of a subgraph from the universe of all knowledge.
-
-    :param pybel.BELGraph universe: The universe of knowledge
-    :param pybel.BELGraph graph: A BEL graph
-    """
+def update_context(universe: BELGraph, graph: BELGraph):
+    """Update the context of a subgraph from the universe of all knowledge."""
     for namespace in get_namespaces(graph):
         if namespace in universe.namespace_url:
             graph.namespace_url[namespace] = universe.namespace_url[namespace]
